2742a346120e6f07e70f2ee14367e58509e6fdd7
iAxce1l <113511385+iAxce1l@users.noreply.github.com>
2023-03-23 13:22:20 +0100

    nav

---
+++ Menu grid zadanie/scripts/script.js
@@ -0,0 +1,10 @@
+// const TV = document.querySelector(".TV")
+// const TV1 = document.querySelector(".TV1")
+
+// TV.addEventListener("click", () => {
+//     if (TV1.style.display == "initial") {
+//       TV1.style.display = "none";
+//     } else {
+//       TV1.style.display = "initial";
+//     }
+//   });
