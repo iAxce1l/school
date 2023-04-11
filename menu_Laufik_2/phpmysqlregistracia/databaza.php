0962728658be7814b045cfa92142cb1bf89ba8bb
iAxce1l <113511385+iAxce1l@users.noreply.github.com>
2023-04-11 21:20:53 +0200

    sql

---
+++ menu_Laufik_2/phpmysqlregistracia/databaza.php
@@ -0,0 +1,12 @@
+<?php
+    //všetky údaje nájdete vo Webglobe -> Hosting -> Databázy
+    //nižšie doplniť vlastné údaje podľa schémy -> mysqli_connect("servername", "username", "password", "databasename");
+    //servername je v tvare edudb.....nameserver.sk
+    $con = mysqli_connect(" ", " ", " ", " ");
+    if (mysqli_connect_errno()){
+        echo "Nepodarilo sa pripojiť k databáze MySQL: " . mysqli_connect_error();
+    }
+?>
+
+
+
