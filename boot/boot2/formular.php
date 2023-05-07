16e8744b778ea3ac5292f1000422f26dd378938c
iAxce1l <113511385+iAxce1l@users.noreply.github.com>
2023-05-07 21:10:17 +0200

    funkčny formular aj s DB

--- boot/boot2/formular.php
+++ boot/boot2/formular.php
@@ -18,3 +18,34 @@ header('Location: index.php');
 
 }
 ?>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // pripojenie
+    $servername = 'edudb-02.nameserver.sk';
+    $username = 'davidlaufik_stu1';
+    $password = 'odTWxPjI3';
+    $dbname = 'davidlaufik_odp';
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // kontrola pripojenia
+    if ($conn->connect_error) {
+        die('Nepripojil si sa na DB: ' . $conn->connect_error);
+    }
+
+    // vkladanie dát
+    $meno = $_POST['meno'];
+    $priezvisko = $_POST['priezvisko'];
+    $email = $_POST['email'];
+    $sprava = $_POST['sprava'];
+    $sql = "INSERT INTO udaje (meno, priezvisko, email, sprava) VALUES ('$meno', '$priezvisko', '$email', '$sprava')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo 'Dáta boli uložené';
+    } else {
+        echo 'Došlo k chybe: ' . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
