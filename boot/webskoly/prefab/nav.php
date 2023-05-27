3e03621d18413d1ae20642b942bc726d74b2569d
iAxce1l <113511385+iAxce1l@users.noreply.github.com>
2023-05-27 22:16:37 +0200

    updated nav

--- boot/webskoly/prefab/nav.php
+++ boot/webskoly/prefab/nav.php
@@ -102,3 +102,31 @@
         </div>
       </div>
     </nav>
+
+    <div
+      class="position-absolute top-10 start-50 translate-middle-x pt-3 position-absolute z-3"
+      id="liveAlertPlaceholder"
+    ></div>
+
+
+    <script>
+      const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
+      const appendAlert = (message, type) => {
+        const wrapper = document.createElement("div");
+        wrapper.innerHTML = [
+          `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+          `   <div>${message}</div>`,
+          '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+          "</div>",
+        ].join("");
+
+        alertPlaceholder.append(wrapper);
+      };
+
+      const alertTrigger = document.getElementById("liveAlertBtn");
+      if (alertTrigger) {
+        alertTrigger.addEventListener("click", () => {
+          appendAlert("Klikol si na search", "success");
+        });
+      }
+    </script>
